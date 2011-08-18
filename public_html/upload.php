079f3697c01943b0c4e9df46a4ff25d0b9afe9c0
Chris McCreadie <chris@cjmcsoftware.co.uk>
2011-08-18 02:10:21 +0100

    ddd

--- public_html/upload.php
+++ public_html/upload.php
@@ -43,8 +43,8 @@ class UploadHandler
                 */
                 'thumbnail' => array(
                   
-            'upload_dir' => '/home/psd2png/public_html/thumbnails/',
-            'upload_url' => 'http://psd2png.fluxflex.com/thumbnails/','max_width' => 80,
+            'upload_dir' => '/home/psd2png/public_html/thumbnails',
+            'upload_url' => 'http://psd2png.fluxflex.com/thumbnails','max_width' => 80,
                     'max_height' => 80
                 )
             )
