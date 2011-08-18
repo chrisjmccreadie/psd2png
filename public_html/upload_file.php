58419b95263a4a7d7f24e012851da53bd35a08f7
Chris McCreadie <chris@cjmcsoftware.co.uk>
2011-08-18 02:26:46 +0100

    cxxcv

--- public_html/upload_file.php
+++ public_html/upload_file.php
@@ -5,15 +5,17 @@ if ($_FILES["file"]["error"] > 0)
   }
 else
   {
-  //echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-  //echo "Type: " . $_FILES["file"]["type"] . "<br />";
-  //echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-  //echo "Stored in: " . $_FILES["file"]["tmp_name"];
-  
+  echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+  echo "Type: " . $_FILES["file"]["type"] . "<br />";
+  echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+  echo "Stored in: " . $_FILES["file"]["tmp_name"];
+  /*
   $im = new Imagick('$_FILES["file"]["tmp_name"]');
 		$im->flattenImages();
 		$im->setImageFormat('png');
         header( 'Content-Type: image/png' );
         echo $im;
   }
+   */
+   */
 ?>
