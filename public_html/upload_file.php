7c1b079685fc692cda22b93987b6a3f4fbb750a8
Chris McCreadie <chris@cjmcsoftware.co.uk>
2011-08-18 02:53:22 +0100

    cc

--- public_html/upload_file.php
+++ public_html/upload_file.php
@@ -5,23 +5,22 @@ if ($_FILES["file"]["error"] > 0)
   }
 else
   {
-  echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-  echo "Type: " . $_FILES["file"]["type"] . "<br />";
-  echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-  echo "Stored in: " . $_FILES["file"]["tmp_name"];
+ // echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+  //echo "Type: " . $_FILES["file"]["type"] . "<br />";
+  //echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+  //echo "Stored in: " . $_FILES["file"]["tmp_name"];
   
-  print_r($_FILES['file']);
+  //print_r($_FILES['file']);
      $res = move_uploaded_file($_FILES["file"]["tmp_name"],'/home/psd2png/public_html/output.psd');
-   echo $res;
+  // echo "res:".$res;
   
-  /*
-  $im = new Imagick('$_FILES["file"]["tmp_name"]');
+  
+  $im = new Imagick('/home/psd2png/public_html/output.psd');
 		$im->flattenImages();
 		$im->setImageFormat('png');
         header( 'Content-Type: image/png' );
         echo $im;
-   * 
-   */
+  
   }
    
    
